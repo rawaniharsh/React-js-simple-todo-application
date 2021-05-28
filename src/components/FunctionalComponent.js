35675844f89db10c156eea7046f47f864d940e31
Harsh <praveengupta@Praveens-MacBook-Pro.local>
2021-05-28 22:17:34 +0530

    first commit

---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+function Funct(){
+        return(
+            <div>
+                <h1>Hello functional component  Walo</h1>
+            </div>
+        );
+}
+
+export default Funct;
